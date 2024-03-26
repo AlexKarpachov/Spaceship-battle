b6737372683b05ab1ad39b1fe2b0e2132da7095d
AlexKarpachov <146717790+AlexKarpachov@users.noreply.github.com>
2024-03-26 20:35:13 +0100

    3 waves are finished

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,30 +1,90 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    Rigidbody enemyRb;
     Scoreboard scoreBoard;
 
-    [SerializeField] int scorePerHit = 5;
+    [SerializeField] int enemyHealth = 100;
+    [SerializeField] int enemyHPDecrease;
+    [SerializeField] int enemyScore = 10;
+    [SerializeField] ParticleSystem hitVFX;
     [SerializeField] ParticleSystem enemyExplosion;
 
     private void Start()
     {
         scoreBoard = FindObjectOfType<Scoreboard>();
+        enemyRb = gameObject.AddComponent<Rigidbody>();
+        enemyRb.useGravity = false;
     }
     private void OnParticleCollision(GameObject other)
     {
-        //Debug.Log("Hitted");
-        scoreBoard.ScoreCalculation(scorePerHit);
-        StartCoroutine(EnemyDestroy());
+
+        if (gameObject.CompareTag("EnemySmall"))
+        {
+            EnemySmallHitting();
+        }
+        else if (gameObject.CompareTag("EnemyMedium"))
+        {
+            EnemyMediumHitting();
+        }
+        else if (gameObject.CompareTag("EnemyLarge"))
+        {
+            EnemyLargeHitting();
+        }
+    }
+
+    private void EnemySmallHitting()
+    {
+        //hitVFX.Play();
+        Debug.Log($"EnemySmall HP is {enemyHealth}");
+        if (enemyHealth > 0)
+        {
+            enemyHealth -= enemyHPDecrease;
+        }
+        if (enemyHealth < 1)
+        {
+            StartCoroutine(EnemyDestroy());
+            //scoreBoard.ScoreCalculation(enemyScore); --- Moved this line to the EnemyDestroy()
+        }
+    }
+
+    void EnemyMediumHitting()
+    {
+        hitVFX.Play();
+        Debug.Log($"EnemyMedium HP is {enemyHealth}");
+        if (enemyHealth > 0)
+        {
+            enemyHealth -= enemyHPDecrease;
+        }
+        if (enemyHealth < 1)
+        {
+            StartCoroutine(EnemyDestroy());
+            //scoreBoard.ScoreCalculation(enemyScore); --- Moved this line to the EnemyDestroy()
+        }
+    }
+
+    void EnemyLargeHitting()
+    {
+        hitVFX.Play();
+        Debug.Log($"EnemyLarge HP is {enemyHealth}");
+        if (enemyHealth > 0)
+        {
+            enemyHealth -= enemyHPDecrease;
+        }
+        if (enemyHealth < 1)
+        {
+            StartCoroutine(EnemyDestroy());
+            //scoreBoard.ScoreCalculation(enemyScore); --- Moved this line to the EnemyDestroy()
+        }
     }
 
     IEnumerator EnemyDestroy()
     {
         enemyExplosion.Play();
-        GetComponent<SphereCollider>().enabled = false;
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.1f);
         Destroy(gameObject);
+        scoreBoard.ScoreCalculation(enemyScore);
     }
 }
--- Assets/Scripts/Score board.cs
+++ Assets/Scripts/Score board.cs
@@ -1,14 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Scoreboard : MonoBehaviour
 {
     int score;
+    TextMeshProUGUI scoreText;
 
+    private void Start()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+        scoreText.text = "Score: 0";
+    }
     public void ScoreCalculation(int addToScore)
     {
         score += addToScore;
-        Debug.Log($"Current score is {score}");
+        scoreText.text = "Score: " + score.ToString();
     }
 }
